522e8813d3e58b80463ef59be3be4d497dc69104
lupitacaballero <79343727+lupitacaballero@users.noreply.github.com>
2021-06-01 17:48:40 -0500

    Add files via upload

---
+++ practica 4.1/t_tiempos.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initialscale=1.0" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/estilos.css" />
+    <title>Tabla viajes</title>
+ </head>
+<body>
+    <div class="container">
+       <h1 class="titulo">Tabla de tiempos a otras ciudades desde filadelfia
+</h1>
+       <div class="table-responsive">
+       <table class="table table-hover">
+       <thead class="thead-dark">
+       <tr>
+       <th>Destino</th>
+       <th>Auto</th>
+       <th>Autobus</th>
+       <th>Bicicleta</th>
+  </tr>
+</thead>
+<tbody>
+      <tr>
+      <td>Nueva York</td>
+      <td>1 h 51 min </td>
+      <td>1 h 22 min </td>
+      <td>8 h 53 min </td>
+ </tr>
+<tr>
+      <td>Allentown</td>
+      <td>1 h 11 min </td>
+      <td> - </td>
+      <td>6 h 9 min</td>
+</tr>
+<tr>
+      <td>Reading</td>
+      <td>1 h 12 min</td>
+      <td> - </td>
+      <td>5 h 40 min</td>
+</tr>
+</tbody>
+<tfoot>
+<tr>
+      <th>Promedio</th>
+      <th>1 h 24 min</th>
+      <th>1 h 22 min</th>
+      <th>6 h 54 min</th>
+</tr>
+</tfoot>
+<caption>Tiempo necesario para viajar desde Filadelphia</caption>
+</table>
+</div>
+</div>
+</body>
+</html>
